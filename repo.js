6ee309f75b3664572c376b1b3e9332d71f0db7f5
lazzzaroni <iamnotlazzaroni@gmail.com>
2021-12-27 11:52:21 +0100

    feat:make API call to get the repo

--- repo.js
+++ repo.js
@@ -10,3 +10,20 @@ const repoName = urlParams.get('repo_name');
 const ownerName = urlParams.get('owner_name');
 // console.log(repoName);
 // console.log(ownerName);
+
+// make API call to get the repo
+const loadRepo = async (repoName, ownerName) => {
+  try {
+    const res = await fetch(
+      `https://api.github.com/repos/${ownerName}/${repoName}`
+    );
+
+    const repo = await res.json();
+    // display repo data on the screen
+    display(repo);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadRepo(repoName, ownerName);
