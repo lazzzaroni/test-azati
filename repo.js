6eff5a0da14bc82333684097c19ca833f05d3d1a
lazzzaroni <iamnotlazzaroni@gmail.com>
2021-12-27 11:47:06 +0100

    feat: get parameters from GET request

--- repo.js
+++ repo.js
@@ -1,1 +1,12 @@
 const repo = document.getElementById('repo');
+
+// get parameters from GET request
+const queryString = window.location.search;
+// console.log(queryString);
+
+const urlParams = new URLSearchParams(queryString);
+
+const repoName = urlParams.get('repo_name');
+const ownerName = urlParams.get('owner_name');
+// console.log(repoName);
+// console.log(ownerName);
