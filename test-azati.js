ae627167c2f29e13faa5f64154daf4d46a5fe480
lazzzaroni <iamnotlazzaroni@gmail.com>
2021-12-27 11:28:07 +0100

    feat: add functional to searchbar

--- test-azati.js
+++ test-azati.js
@@ -1,2 +1,15 @@
 const cardList = document.getElementById('cardList');
 const searchBar = document.getElementById('searchBar');
+let reposNames = {};
+
+// add functional to searchbar
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+  // console.log(searchString);
+
+  if (searchString === '') {
+    reposNames = {};
+  } else {
+    loadRepos(searchString);
+  }
+});
